df35f9964c4245c74b5b0a56f445fc5cd151b70c
nicolas237 <nicolas@focomultimidia.com>
2023-12-05 00:53:00 -0300

    docs: adiciona phpdocs na classe Graph

--- src/Graph.php
+++ src/Graph.php
@@ -2,20 +2,39 @@
 
 namespace App;
 
+use App\Exceptions\InvalidEdgeException;
+
 class Graph implements \JsonSerializable
 {
     private array $edges;
-    
+
+    private array $matrix = [];
+
+    private array $vertexes = [];
+
+    /**
+     * Adiciona uma aresta ao grafo
+     * @param Edge $edge Aresta a ser adicionada
+     * @return void
+     */
     public function addEdge(Edge $edge): void
     {
         $this->edges[] = $edge;
     }
 
+    /**
+     * Retorna todas as arestas do grafo
+     * @return array
+     */
     public function getEdges(): array
     {
         return $this->edges;
     }
 
+    /**
+     * Serializa o grafo em JSON estruturado com as arestas, vértices e pesos, caso o grafo seja ponderado
+     * @return string
+     */
     public function jsonSerialize(): string
     {
         $edges = [];
