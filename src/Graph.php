d0fd6623100627dc3559adb7d3aee867b6164989
nicolas237 <nicolas@focomultimidia.com>
2023-12-05 00:53:28 -0300

    feat: implementa método para definição de matriz de adjacências do grafo

--- src/Graph.php
+++ src/Graph.php
@@ -54,4 +54,76 @@ class Graph implements \JsonSerializable
 
         return json_encode($edges);
     }
+
+    /**
+     * Constrói a matriz de adjacência do grafo
+     * @return void
+     */
+    public function buildMatrix(): void
+    {
+        foreach ($this->edges as $edge) {
+            $vertexA = $edge->getVertexA();
+            $vertexB = $edge->getVertexB();
+
+            $vertexAEdges = $edge->getVertexA()->getEdges();
+            $vertexBEdges = $edge->getVertexB()->getEdges();
+
+            $this->processesVertexAdjacency($vertexAEdges, $vertexA);
+            $this->processesVertexAdjacency($vertexBEdges, $vertexB);
+
+            $this->matrix[$vertexA->getValue()][$vertexB->getValue()] = true;
+            $this->matrix[$vertexB->getValue()][$vertexA->getValue()] = true;
+        }
+
+        foreach ($this->vertexes as $vertex) {
+            foreach ($this->vertexes as $vertex2) {
+                if (! isset($this->matrix[$vertex][$vertex2])) {
+                    $this->matrix[$vertex][$vertex2] = false;
+                }
+
+                if ($vertex == $vertex2) {
+                    $this->matrix[$vertex][$vertex2] = true;
+                }
+            }
+        }
+    }
+
+    public function getAdjacencyMatrix(): array
+    {
+        return $this->matrix;
+    }
+
+    /**
+     * @throws InvalidEdgeException
+     */
+    public function addMultipleEdges(array $edges): void
+    {
+        foreach ($edges as $edge) {
+            if (! $edge instanceof Edge) {
+                throw new InvalidEdgeException('O array deve conter apenas objetos do tipo Edge');
+            }
+
+            $this->addEdge($edge);
+        }
+    }
+
+    /**
+     * Processa a adjacência de um vértice a partir de suas arestas
+     * @param array $vertexEdges
+     * @param Vertex $vertex
+     * @return void
+     */
+    private function processesVertexAdjacency(array $vertexEdges, Vertex $vertex): void
+    {
+        foreach ($vertexEdges as $edge) {
+            $this->vertexes[] = $edge['vertexA'];
+            $this->vertexes[] = $edge['vertexB'];
+
+            if ($edge['vertexA'] == $vertex->getValue()) {
+                $this->matrix[$vertex->getValue()][$edge['vertexB']] = true;
+            } else if ($edge['vertexB'] == $vertex->getValue()) {
+                $this->matrix[$vertex->getValue()][$edge['vertexA']] = true;
+            }
+        }
+    }
 }
