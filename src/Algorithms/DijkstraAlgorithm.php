c531f0476fc098246466d1ee66716eb95673d386
nicolas237 <nicolas@focomultimidia.com>
2023-12-08 16:15:53 -0300

    feat: dijkstra, agora foi.

--- src/Algorithms/DijkstraAlgorithm.php
+++ src/Algorithms/DijkstraAlgorithm.php
@@ -24,59 +24,39 @@ class DijkstraAlgorithm
      */
     private function shortestPath(Graph $graph, int $origin, int $destination): void
     {
-        $distances = array_fill(0, count($graph->getVertexes()), INF);
-        $previous = [];
-        $matrix = $graph->getAdjacencyMatrix();
-        $visited = $graph->getVertexes();
+        $distancia = array_fill(0, count($graph->getVertexes()), INF);
+        $previous = array_fill(1, count($graph->getVertexes()), null);
 
-        $distances[$origin] = 0;
+        $distancia[$origin - 1] = 0;
+        $visitados = [];
 
-        while (count($visited) > 0) {
-            $minDistanceVertex = $this->getEdgeWithMinWeight($matrix, $visited);
-            if ($minDistanceVertex == null) {
-                break;
-            }
+        while (count($visitados) != count($distancia)) {
+            $verticeAtual = null;
+            $menorDistancia = INF;
 
-            if ($minDistanceVertex == $destination) {
-                break;
-            }
-
-            foreach ($matrix[$minDistanceVertex] as $neighbor => $weight) {
-                $alt = $distances[$minDistanceVertex] != INF ? $distances[$minDistanceVertex] + $weight : $weight;
-                if ($alt < $distances[$neighbor]) {
-                    $distances[$neighbor] = $alt;
-                    $previous[$neighbor] = $minDistanceVertex;
+            for ($i = 0; $i < count($graph->getVertexes()); $i++) {
+                if (! in_array($i, $visitados) && $distancia[$i] < $menorDistancia) {
+                    $verticeAtual = $i;
+                    $menorDistancia = $distancia[$i];
                 }
             }
 
-            unset($visited[$minDistanceVertex]);
-        }
-
-        dd($distances, $previous);
-    }
-
-    /**
-     * Retorna a aresta com o menor peso
-     *
-     * @param array $matrix Array de adjacência
-     * @return int|null
-     */
-    public function getEdgeWithMinWeight(array $matrix, array $visited): ?int
-    {
-        $min = INF;
-        $minDistanceVertex = null;
-
-        foreach ($visited as $vertex) {
-            foreach ($matrix[$vertex] as $adjacentVertex => $weight) {
-                if ($weight === 0) continue;
-
-                if ($weight < $min && $vertex != $adjacentVertex && ! empty($visited[$adjacentVertex])) {
-                    $min = $weight;
-                    $minDistanceVertex = $adjacentVertex;
+            $visitados[] = $verticeAtual;
+            foreach ($graph->getAdjacencyMatrix()[$verticeAtual + 1] as $vizinho => $peso) {
+                if ($distancia[$verticeAtual] + $peso < $distancia[$vizinho - 1] && $peso != 0) {
+                    $distancia[$vizinho - 1] = $distancia[$verticeAtual] + $peso;
+                    $previous[$vizinho] = $verticeAtual + 1;
                 }
             }
         }
 
-        return $minDistanceVertex;
+        $path = [];
+        $current = $destination;
+        while (!is_null($current)) {
+            array_unshift($path, $current);
+            $current = $previous[$current];
+        }
+
+        $this->shortestPath = $path;
     }
 }
